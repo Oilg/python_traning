9dc0eac4e452e06ff14ecca9af53c6dbcbf8fd49
Oilg <50927525+Oilg@users.noreply.github.com>
2020-04-23 21:30:51 +0300

    conftest и удаление группы
    Вынесение фикстуры в отдельный файл, добавление в нее scope = session для запуска тестов в одном браузере.
    Добавление теста на удаление группы

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,17 +1,10 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.contact import Contact
-from fixture.application import Application
-
-
-@pytest.fixture()
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_contact(app):
     app.session.login(username='admin', password='secret')
-    app.contact.create(Contact(firstname='Анзор', lastname='Шарипов', title='Лалка', address='В горах жи есть'))
+    app.contact.create(Contact(firstname='Анзор', lastname='Шарипов',
+                               title='Лалка', address='В горах жи есть',
+                               email='azaza@mail.ru', mobile='11'))
     app.session.logout()
