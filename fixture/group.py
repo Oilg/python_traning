905ebdab02eba6769e58d37ecf55805a9d5a5dba
Oilg <topgun92@mail.ru>
2020-04-28 15:52:26 +0300

    Сокращено время выполнения тестов
    1. Добавлено условие, определяющее текущую страницу, если не та - переход на нужную.
    2. Убрана задаржка по времени для прогрузки страницы (в Application)

--- fixture/group.py
+++ fixture/group.py
@@ -4,7 +4,8 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text('groups').click()
+        if not (wd.current_url.endswith('/group.php') and len(wd.find_elements_by_name('new')) > 0):
+            wd.find_element_by_link_text('groups').click()
 
     def create(self, group):
         wd = self.app.wd
@@ -17,7 +18,6 @@ class GroupHelper:
         self.return_to_groups()
 
     def fill_group_form(self, group):
-        wd = self.app.wd
         self.change_field_value('group_name', group.name)
         self.change_field_value('group_header', group.header)
         self.change_field_value('group_footer', group.footer)
