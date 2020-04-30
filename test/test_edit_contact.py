815cb87e9dcf28b9c34acabba776767b3c71ffbe
Oilg <topgun92@mail.ru>
2020-04-30 09:46:11 +0300

    Доработан тест изменения имени контакта
    Добавлена новая проверка изменения имени контакта, путем сравнения списка контактов до изменения и после изменения. (работает, если имя при изменении введено одним словом)

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -6,9 +6,13 @@ def test_edit_contact_firstname(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname='Анзор', lastname='Шарипов'))
     old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact(firstname='Новое имя'))
+    contact = Contact(firstname='Новое')
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_edit_contact_lastname(app):
