502746091e17b67e996d1cda1a328bea163638b5
Egor Loginov <topgun92@mail.ru>
2020-05-10 18:01:23 +0300

    список контактов из БД
    Создан метод get_contact_list в DbFixture

--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from model.group import Group
+from model.contact import Contact
 
 
 class DbFixture:
@@ -10,6 +11,8 @@ class DbFixture:
         self.user = user
         self.password = password
         self.connection = mysql.connector.connect(host=host, database=name, user=user, password=password)
+        self.connection.autocommit = True  # отключаем кэширование БД, что бы в тестах не было различий
+                                          # (старые списки и новые)
 
     def get_group_list(self):
         list = []
@@ -19,6 +22,21 @@ class DbFixture:
             for row in cursor:
                 (id, name, header, footer) = row
                 list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute(
+                'select id, firstname, lastname, title, address, home, mobile, work, email, phone2 from addressbook')
+            for row in cursor:
+                (id, firstname, lastname, title, address, home, mobile, work, email, phone2) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, title=title, address=address,
+                                    homephone=home, mobilephone=mobile, workphone=work, secondaryphone=phone2,
+                                    email=email))
         finally:
             cursor.close()
         return list
